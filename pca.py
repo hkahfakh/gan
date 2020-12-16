6ee2fd01f43168580ea449939b61c90f2a74b735
hkahfakh <you@example.com>
2020-12-16 19:12:34 +0800

    实现了PCA降维，并使用KNN进行了测试获得了好效果

---
+++ pca.py
@@ -0,0 +1,30 @@
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from getData import get_data, data_split
+
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+
+def knn_classifier(train_X, train_y, test_X, test_y):
+    knn = KNeighborsClassifier(n_jobs=-1)
+    knn.fit(train_X, train_y)
+    result = knn.predict(test_X)
+    return accuracy_score(result, test_y), metrics.recall_score(result, test_y, average='micro')
+
+# 返回降维后的特征
+def dimensionReduction(dim=10):
+    dim = 10
+    pca = PCA(n_components=dim)
+    # X_pca:降维后的X
+    X_pca = pca.fit_transform(X)
+    print(pca.noise_variance_)
+    return X_pca
+
+if __name__ == '__main__':
+    data = get_data("./dataSet/UNSW_finally.npy")
+    X, y = data[:, :-1], data[:, -1]
+    X_pca = dimensionReduction(10)
+    X_train,X_test,y_train,y_test = train_test_split(X_pca, y, test_size=0.2)
+    print(knn_classifier(X_train, y_train, X_test, y_test))
