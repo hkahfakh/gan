c98bb14a15ebc03af765f223ffa092570ca51fd1
hkahfakh <you@example.com>
2020-12-17 16:48:06 +0800

    能够正常运行了

--- gan.py
+++ gan.py
@@ -1,66 +1,60 @@
 # coding:utf-8
-'''
-DCGAN on MNIST using Keras
-Author: Rowel Atienza
-Project: https://github.com/roatienza/Deep-Learning-Experiments
-Dependencies: tensorflow 1.0 and keras 2.0
-Usage: python3 dcgan_mnist.py
-'''
-
 import numpy as np
 import time
 from tensorflow.examples.tutorials.mnist import input_data
 
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Reshape
-from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
-from keras.layers import LeakyReLU, Dropout
+from keras.models import Sequential, Model
+from keras.layers import Dense, Activation, Flatten, Reshape, Input
 from keras.layers import BatchNormalization
 from keras.optimizers import Adam, RMSprop
 
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from getData import get_data
+from pca import dimensionReduction
+from tqdm import tqdm
+
+from keras.utils.vis_utils import plot_model
 
 
-class ElapsedTimer(object):
-    def __init__(self):
-        self.start_time = time.time()
-
-    def elapsed(self, sec):
-        if sec < 60:
-            return str(sec) + " sec"
-        elif sec < (60 * 60):
-            return str(sec / 60) + " min"
-        else:
-            return str(sec / (60 * 60)) + " hr"
-
-    def elapsed_time(self):
-        print("Elapsed: %s " % self.elapsed(time.time() - self.start_time))
-
-
-
-def pcaDimensionreduction():
-    pass
-
-class DCGAN(object):
-    def __init__(self, input_rows=1, input_cols=28):
-
+class GAN(object):
+    # 输出的col看降维的维数
+    def __init__(self, input_rows=1, input_cols=16):
         self.input_rows = input_rows
         self.input_cols = input_cols
         self.D = None  # discriminator
         self.G = None  # generator
-        self.AM = None  # adversarial model
         self.DM = None  # discriminator model
         self.GM = None  # generator model
+        self.AM = None  # adversarial model
+        self.AM_dual = None  # adversarial model
+
+    # 这意思应该是生成16位的特征   供辨别器分辨
+    def generator(self):
+        if self.G:  # 判断是否已经创建了
+            return self.G
+        self.G = Sequential(name='generator')
+
+        self.G.add(Dense(64, input_shape=(16,)))
+        self.G.add(BatchNormalization(momentum=0.9))
+        self.G.add(Activation('relu'))
+
+        self.G.add(Dense(32))
+        self.G.add(BatchNormalization(momentum=0.9))
+        self.G.add(Activation('relu'))
+
+        self.G.add(Dense(16))
+        self.G.add(BatchNormalization(momentum=0.9))
+        self.G.add(Activation('relu'))
+
+        return self.G
 
     # (W−F+2P)/S+1
     def discriminator(self):
         if self.D:
             return self.D
-        self.D = Sequential(name='discriminator')  # 创建一个构造模型
 
-        # In: 1 x 28 x 1, depth = 1    输入通道数
-        # Out: 14 x 14 x 1, depth=64   输出通道数
-        input_shape = (self.input_rows, self.input_cols)
+        self.D = Sequential(name='discriminator')  # 创建一个构造模型
 
         self.D.add(Dense(64, input_shape=(16,)))  # 输入 (*, 16) 的数组    输出(*, 32)
         self.D.add(BatchNormalization(momentum=0.9))
@@ -74,30 +68,18 @@ class DCGAN(object):
         self.D.add(BatchNormalization(momentum=0.9))
         self.D.add(Activation('relu'))
 
-        self.D.summary()
         return self.D
 
-    def generator(self):
-        if self.G:  # 判断是否已经创建了
-            return self.G
-        self.G = Sequential(name='generator')
+    # 输出的是正常或恶意数据的概率
+    def generator_model(self):
+        if self.GM:
+            return self.GM
 
-        self.G.add(Dense(64, input_shape=(self.input_rows, self.input_cols)))
-        self.G.add(BatchNormalization(momentum=0.9))
-        self.G.add(Activation('relu'))
-
-        # In: dim x dim x depth
-        # Out: 2*dim x 2*dim x depth/2
-        self.G.add(Dense(32))
-        self.G.add(BatchNormalization(momentum=0.9))
-        self.G.add(Activation('relu'))
-
-        self.G.add(Dense(16))
-        self.G.add(BatchNormalization(momentum=0.9))
-        self.G.add(Activation('relu'))
-
-        self.G.summary()  # 打印模型概述信息
-        return self.G
+        optimizer = Adam(lr=0.001, decay=0.999)
+        self.GM = Sequential(name="generator_model")
+        self.GM.add(self.generator())
+        self.GM.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        return self.GM
 
     # 损失函数是    没整明白
     def discriminator_model(self):
@@ -105,103 +87,84 @@ class DCGAN(object):
             return self.DM
 
         optimizer = Adam(lr=0.001, decay=0.9)
-        self.DM = Sequential()
+        self.DM = Sequential(name="discriminator_model")
         self.DM.add(self.discriminator())
-        self.DM.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        self.DM.add(Dense(1, activation='sigmoid'))  # 二分类所以加了这个
+        self.DM.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])  # 交叉熵就是KL散度
         return self.DM
-
-    # 损失函数是   类损失+域损失
-    def generator_model(self):
-        if self.GM:
-            return self.GM
-
-        optimizer = Adam(lr=0.001, decay=0.999)
-        self.GM = Sequential()
-        self.GM.add(self.generator())
-        self.GM.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-        return self.GM
 
     # 不能简单的将两个模型接在了一起
     # generator-class classier
-    #         \discriminator_model=(discriminator + domain classier)
+    # generator-discriminator_model=(discriminator + domain classier)
     def adversarial_model(self):
         if self.AM:
             return self.AM
-        # optimizer = RMSprop(lr=0.0001, decay=3e-8)
-        optimizer = Adam(lr=0.001, decay=0.999)
-        self.AM = Sequential()
-        self.AM.add(self.generator())
-        self.AM.add(self.discriminator())
-        self.AM.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])  # 交叉熵就是KL散度
+
+        self.DM.trainable = False
+        GAN_input = Input(shape=(16,))
+        x = self.GM(GAN_input)
+        GAN_output = self.DM(x)
+        self.AM = Model(inputs=GAN_input, outputs=GAN_output)
+        self.AM.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.5))
         return self.AM
 
 
-class MNIST_DCGAN(object):
-    def __init__(self):
-        self.input_rows = 1
-        self.input_cols = 28
+def train_GAN(gan, epochs=300, batch_size=32):
+    # Loading the data
+    UNSW_data = get_data("./dataSet/UNSW_finally.npy")
+    X, y = UNSW_data[:, :-1], UNSW_data[:, -1]
+    X_pca = dimensionReduction(X, 16)
+    UNSW_X_train, UNSW_X_test, UNSW_y_train, UNSW_y_test = train_test_split(X_pca, y,
+                                                                            test_size=0.2)
 
-        self.x_train = input_data.read_data_sets("mnist", one_hot=True).train.images
-        self.x_train = self.x_train.reshape(-1, self.input_rows, self.input_cols, 1).astype(np.float32)
+    KDD_data = get_data("./dataSet/KDD_finally.npy")
+    X, y = KDD_data[:, :-1], KDD_data[:, -1]
+    X_pca = dimensionReduction(X, 16)
+    KDD_X_train, KDD_X_test, KDD_y_train, KDD_y_test = train_test_split(X_pca, y, test_size=0.2)
 
-        self.DCGAN = DCGAN()
-        self.discriminator = self.DCGAN.discriminator_model()
-        self.adversarial = self.DCGAN.adversarial_model()
-        self.generator = self.DCGAN.generator()
+    # Creating GAN
+    generator = gan.generator_model()
+    discriminator = gan.discriminator_model()
+    GAN = gan.adversarial_model()
 
-    def train(self, train_steps=2000, batch_size=256, save_interval=0):
-        noise_input = None
-        if save_interval > 0:
-            noise_input = np.random.uniform(-1.0, 1.0, size=[16, 100])
-        for i in range(train_steps):
-            images_train = self.x_train[np.random.randint(0, self.x_train.shape[0], size=batch_size), :, :, :]
-            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
-            images_fake = self.generator.predict(noise)
-            x = np.concatenate((images_train, images_fake))
-            y = np.ones([2 * batch_size, 1])
-            y[batch_size:, :] = 0
-            d_loss = self.discriminator.train_on_batch(x, y)
+    for i in range(1, epochs + 1):
+        print("Epoch %d" % i)
 
-            y = np.ones([batch_size, 1])
-            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
-            a_loss = self.adversarial.train_on_batch(noise, y)
-            log_mesg = "%d: [D loss: %f, acc: %f]" % (i, d_loss[0], d_loss[1])
-            log_mesg = "%s  [A loss: %f, acc: %f]" % (log_mesg, a_loss[0], a_loss[1])
-            print(log_mesg)
-            if save_interval > 0:
-                if (i + 1) % save_interval == 0:
-                    self.plot_images(save2file=True, samples=noise_input.shape[0],
-                                     noise=noise_input, step=(i + 1))
+        for _ in tqdm(range(batch_size)):
+            # Generate fake images from random noiset
+            target_data = UNSW_X_train[np.random.randint(0, UNSW_X_train.shape[0], batch_size)]
+            target_data = generator.predict(target_data)
+            # Select a random batch of real images from MNIST
+            source_data = KDD_X_train[np.random.randint(0, KDD_X_train.shape[0], batch_size)]
+            source_data = generator.predict(source_data)
 
-    def plot_images(self, save2file=False, fake=True, samples=16, noise=None, step=0):
-        filename = 'output/mnist.png'
-        if fake:
-            if noise is None:
-                noise = np.random.uniform(-1.0, 1.0, size=[samples, 100])
-            else:
-                filename = "mnist_%d.png" % step
-            images = self.generator.predict(noise)
-        else:
-            i = np.random.randint(0, self.x_train.shape[0], samples)
-            images = self.x_train[i, :, :, :]
+            # 假的图片为标签一串0   真的图片标签一串1
+            label_target = np.zeros(batch_size)
+            label_source = np.ones(batch_size)
 
-        plt.figure(figsize=(10, 10))
-        for i in range(images.shape[0]):
-            plt.subplot(4, 4, i + 1)
-            image = images[i, :, :, :]
-            image = np.reshape(image, [self.input_rows, self.input_cols])
-            plt.imshow(image, cmap='gray')
-            plt.axis('off')
-        plt.tight_layout()
-        if save2file:
-            plt.savefig(filename)
-            plt.close('all')
-        else:
-            plt.show()
+            # Concatenate fake and real images
+            X = np.concatenate([target_data, source_data])
+            y = np.concatenate([label_target, label_source])
+
+            # Train the discriminator
+            discriminator.trainable = True
+            discriminator.train_on_batch(X, y)
+
+            # Train the generator/chained GAN model (with frozen weights in discriminator)
+            discriminator.trainable = False
+            loss =GAN.train_on_batch(target_data, label_source)
+    # print(generator.evaluate(UNSW_X_train,  np.zeros(UNSW_X_train.shape[0])))
+
+    target_data = UNSW_X_train[np.random.randint(0, UNSW_X_train.shape[0], 1)]
+    target_data = generator.predict(target_data)
+    print("为源数据集的概率", discriminator.predict(target_data))
 
 
+# NSL-KDD作为源数据集，UNSW-NB15作为目标数据集
+# 两个数据集都转换到一个特征空间
 if __name__ == '__main__':
-    # mnist_dcgan = MNIST_DCGAN()
-    # mnist_dcgan.train(train_steps=10, batch_size=256, save_interval=500)
-    # mnist_dcgan.plot_images(fake=True)
-    # mnist_dcgan.plot_images(fake=False, save2file=True)
+    # nids_gan = NIDS_GAN()
+    # nids_gan.train(train_steps=16, batch_size=32, save_interval=500)
+    # nids_gan.plot_images(fake=True)
+    # nids_gan.plot_images(fake=False, save2file=True)
+    train_GAN(GAN())
