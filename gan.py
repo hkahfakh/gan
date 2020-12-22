0c3ed37fce78e06cfebe9e05fb9e3c542cfa7b44
hkahfakh <you@example.com>
2020-12-22 15:11:03 +0800

    加完loss能运行了 但是loss nan

--- gan.py
+++ gan.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 from getData import get_data
 from pca import dimensionReduction
 
-from keras.utils.vis_utils import plot_model
+# from keras.utils.vis_utils import plot_model
 import tensorflow as tf
 from keras import backend as K
 
@@ -20,10 +20,7 @@ class GAN(object):
     def __init__(self, input_rows=1, input_cols=16):
         self.input_rows = input_rows
         self.input_cols = input_cols
-        self.E_source_mean = 0
-        self.E_target_mean = 0
-        self.E_attack_mean = 0
-        self.E_benign_mean = 0
+        self.DGxt = None
         self.D = None  # discriminator
         self.G = None  # generator
         self.DM = None  # discriminator model
@@ -72,35 +69,25 @@ class GAN(object):
 
         return self.D
 
-    def class_loss(self, y_true, y_pred):
-        E_attack = np.float32(self.E_attack_mean)
-        E_benign = np.float32(self.E_benign_mean)
-        E_attack = tf.convert_to_tensor(E_attack)
-        E_benign = tf.convert_to_tensor(E_benign)
-        a = -E_attack * (K.log(y_pred)) - E_benign * (K.log(1 - y_pred))
-        return a
+    # 目标 + 源
+    def discriminator_loss(self, y_true, y_pred):
+        a = -K.mean(K.log(y_pred[16:]))
+        b = K.log(1 - y_pred[:16])
+        return a + b
 
-    def domain_loss(self, y_true, y_pred):
-        E_source = self.E_source_mean
-        E_target = self.E_target_mean
-        E_source = tf.convert_to_tensor(E_source)
-        E_target = tf.convert_to_tensor(E_target)
-        return -E_source * (K.log(y_pred)) - E_target * (K.log(1 - y_pred))
-
-    def binary_crossentropy(self, y_true, y_pred):
-        return K.mean(K.binary_crossentropy(y_pred, y_true), axis=-1)
+    # 这个是类分类 的loss    所以真实值预测值   是是否为恶意攻击的概率
+    def generator_loss(self, y_true, y_pred):
+        L_class = -K.mean(y_true*(K.log(y_pred))) + (1-y_true)*(K.log(1 - y_pred))
+        L_domain = -K.mean(K.log(self.DGxt))# 这个地方不能用y_pred
+        return (L_class + L_domain) / 2
 
     # 输出的是正常或恶意数据的概率
     def generator_model(self):
         if self.GM:
             return self.GM
 
-        optimizer = Adam(lr=0.001, decay=0.999)
         self.GM = Sequential(name="generator_model")
         self.GM.add(self.generator())
-        self.GM.compile(loss=self.class_loss,
-                        optimizer=optimizer,
-                        metrics=['accuracy'])
         return self.GM
 
     # 损失函数是    没整明白
@@ -110,25 +97,12 @@ class GAN(object):
 
         optimizer = Adam(lr=0.001, decay=0.9)
         self.DM = Sequential(name="discriminator_model")
+        self.DM.add(self.generator())
         self.DM.add(self.discriminator())
         self.DM.add(Dense(1, activation='sigmoid'))  # 二分类所以加了这个
-        self.DM.compile(loss=self.domain_loss, optimizer=optimizer, metrics=['accuracy'])  # 交叉熵就是KL散度
+        self.DM.compile(loss=self.discriminator_loss, optimizer=optimizer, metrics=['accuracy'])  # 交叉熵就是KL散度
         return self.DM
 
-    # 不能简单的将两个模型接在了一起
-    # generator-class classier
-    # generator-discriminator_model=(discriminator + domain classier)
-    def adversarial_model(self):
-        if self.AM:
-            return self.AM
-
-        self.DM.trainable = False
-        GAN_input = Input(shape=(16,))
-        x = self.GM(GAN_input)
-        GAN_output = self.DM(x)
-        self.AM = Model(inputs=GAN_input, outputs=GAN_output)
-        self.AM.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.5))
-        return self.AM
 
     def classifer_model(self):
         if self.CM:
@@ -138,8 +112,8 @@ class GAN(object):
         self.CM = Sequential(name="classifer_model")
         self.CM.add(self.generator())
         self.CM.add(Dense(1, activation='sigmoid'))  # 二分类所以加了这个
-        self.CM.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-        plot_model(self.CM, to_file='output/classifer.png', show_shapes=True)
+        self.CM.compile(loss=self.generator_loss, optimizer=optimizer, metrics=['accuracy'])
+        # plot_model(self.CM, to_file='output/classifer.png', show_shapes=True)
         return self.CM
 
 
@@ -170,20 +144,24 @@ def testDomainClassifer(X, dataSetName, gen, disc):
     print("数据集为", dataSetName)
 
 
-def train_GAN(gan, source_dataSet, target_dataSet, epochs=300, batch_size=32, loadWeights=1):
+def train_GAN(gan, source_dataSet, target_dataSet, epochs=10000, batch_size=32, loadWeights=0):
     # Loading the data
     UNSW_X_train, UNSW_X_test, UNSW_y_train, UNSW_y_test = target_dataSet
     KDD_X_train, KDD_X_test, KDD_y_train, KDD_y_test = source_dataSet
 
-    # Creating GAN
     generator = gan.generator_model()
-    classification = gan.classifer_model()
     discriminator = gan.discriminator_model()
-    GAN = gan.adversarial_model()
+    target_data = UNSW_X_train[:16]
+    target_data = generator.predict(target_data)
+    gan.DGxt = discriminator.predict(target_data)
+    gan.DGxt = gan.DGxt.astype(np.float32).flatten()
+    gan.DGxt = tf.convert_to_tensor(gan.DGxt, tf.float32)
+    classification = gan.classifer_model()
+
 
     if loadWeights == 1:  # genertor是共享权重的 权重相同 可以打印权重确认
-        GAN.load_weights("./output/gan_weight.h5", by_name=True)
-        classification.load_weights('./output/class_classifer_weight.h5')
+        discriminator.load_weights("./output/gan_weight7400.h5", by_name=True)
+        classification.load_weights('./output/class_classifer_weight7400.h5')
     else:
         for e in range(0, epochs):
             lower_bound = int(e * batch_size / 2)
@@ -207,27 +185,29 @@ def train_GAN(gan, source_dataSet, target_dataSet, epochs=300, batch_size=32, lo
             X = np.concatenate([target_data, source_data])
             y = np.concatenate([label_target, label_source])
 
+            gan.DGxt = discriminator.predict(target_data)
+            gan.DGxt = gan.DGxt.astype(np.float32).flatten()
+            gan.DGxt = tf.convert_to_tensor(gan.DGxt, tf.float32)
+
             # 训练辨别器
             generator.trainable = False
             discriminator.trainable = True
-            discriminator.train_on_batch(X, y)
-            discriminator.fit(X, y, initial_epoch=e, epochs=e + 1, verbose=1)
+            discriminator.fit(target_data, label_source, initial_epoch=e, epochs=e + 1, verbose=1)
 
             # 训练生成器(此时冻结辨别器权重)
             generator.trainable = True
             discriminator.trainable = False
-            GAN.fit(target_data, label_source, initial_epoch=e, epochs=e + 1, verbose=1)
             classification.fit(class_X_train, class_y_train, initial_epoch=e, epochs=e + 1, verbose=1)
 
-            # if epochs % 10 == 0:
-            #     classification.save_weights('./output/class_classifer_weight.h5')
-            #     GAN.save_weights('./output/gan_weight.h5')
+            if e % 200 == 0:
+                classification.save_weights('./output/class_classifer_weight' + str(e) + '.h5')
+                discriminator.save_weights('./output/gan_weight' + str(e) + '.h5')
 
         classification.save_weights('./output/class_classifer_weight.h5')
-        GAN.save_weights('./output/gan_weight.h5')
+        discriminator.save_weights('./output/gan_weight.h5')
 
-    testClassClassifer(KDD_X_train, KDD_y_train, classification)
-    testDomainClassifer(UNSW_X_train, "UNSW_X_train", generator, discriminator)
+    # testClassClassifer(KDD_X_train, KDD_y_train, classification)
+    # testDomainClassifer(UNSW_X_train, "UNSW_X_train", generator, discriminator)
 
 
 # NSL-KDD作为源数据集，UNSW-NB15作为目标数据集
@@ -237,10 +217,4 @@ if __name__ == '__main__':
     target_dataSet = gen_Data("./dataSet/UNSW_finally.npy")
 
     gan = GAN()
-    gan.E_source_mean = 1 / 2
-    gan.E_target_mean = 1 / 2
-    a = np.concatenate([source_dataSet[2], target_dataSet[2]])
-    gan.E_attack_mean = calMean(a)
-    a = 1 - a
-    gan.E_benign_mean = calMean(a)
     train_GAN(gan, source_dataSet, target_dataSet)
